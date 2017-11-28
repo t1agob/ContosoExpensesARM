174df1202000ed45720b381d1afb04c168331fbd
t1agob <tiagob@microsoft.com>
2017-11-28 18:58:28 +0000

    update ps and readme
    Updated readme and ps to make it easier to use

--- README.md
+++ README.md
@@ -1,3 +1,47 @@
 # ContosoExpensesARM
+The purpose of this template is to deploy the complete Contoso Expenses aplication to Azure. The following resources will be deployed.
+* 2 Web Applications (Web and API)
+* 1 Function App
+* 2 Storage Accounts
+* 1 SQL Database
+* 1 SendGrid Email 
+
+This template uses GitHub integration to deploy to the Azure WebApps and Azure Function. It will also restore the database using a _.bacpac_ file.
+
+# Steps do deploy
+1. [Run requirements script](#run-requirements-script)
+2. [Deploy the template](#deploy-the-template)
+3. [Configure SendGrid](#configure-sendgrid)
+
+
+## Run requirements script
+Folder _pre-reqs_ contains a PowerShell script that you need to run in order to copy the database to a storage account in your subcription.
+
+**Run the script and make sure to copy the output** somewhere since it will be used in the template deployment phase.
+
+````POWERSHELL
+C:\..\ContosoExpensesARM\pre-reqs> .\Create-Storage-Account-And-Copy-DB.ps1
+
+Successfully Signed-in
+Created resource group
+Created storage account
+Created storage container
+Successfully uploaded .bacpac to storage account
+
+ ----                  DEPLOYMENT INFORMATION                  ----
+ - RESOURCE GROUP NAME: ContosoExpensesARM1271674349
+ - STORAGE ACCOUNT NAME: sqlimport678651702
+ - STORAGE ACCOUNT KEY: sZ+1FFaHRvoHJYsSlvv5ZK1IetCPWY8LiGLM0hjmwfJWPke5LHz5d2vHc5Q9EOPRmeXp+4AADVAUq5UG7HKkWA==
+ - KEEP THIS INFORMATION AS IT WILL BE REQUIRED FOR DEPLOYING -
+````
+
+
+## Deploy the template
+To deploy the ARM template click the _Deploy to Azure_ button. Fill in the required fields.
+
+> Make sure the information copied previously is used in the this deployment phase.
 
 [![Deploy to Azure](https://azuredeploy.net/deploybutton.svg)](https://azuredeploy.net/)
+
+## Configure SendGrid
+
--- pre-reqs/Create-Storage-Account-And-Copy-DB.ps1
+++ pre-reqs/Create-Storage-Account-And-Copy-DB.ps1
@@ -1,3 +1,6 @@
+param(
+    [string]$subscriptionName
+)
 # VARIABLES
 $resourcegroupname = "ContosoExpensesARM$(Get-Random)"
 $location = "westeurope"
@@ -6,10 +9,17 @@ $storagecontainername = "importcontainer"
 $bacpacfilename = "ContosoExpensesDB.bacpac"
 
 # Login in Azure
-Login-AzureRmAccount
+if($subscriptionName){
+    Login-AzureRmAccount -Subscription $subscriptionName
+}
+else{
+    Login-AzureRmAccount
+}
+Write-host "Successfully Signed-in" -f Cyan
 
 # Create a resource group
 $resourcegroup = New-AzureRmResourceGroup -Name $resourcegroupname -Location $location
+Write-host "Created resource group" -f Cyan
 
 # Create a storage account 
 $storageaccount = New-AzureRmStorageAccount -ResourceGroupName $resourcegroupname `
@@ -17,10 +27,14 @@ $storageaccount = New-AzureRmStorageAccount -ResourceGroupName $resourcegroupnam
     -Location $location `
     -Type "Standard_LRS"
 
+Write-host "Created storage account" -f Cyan
+
 # Create a storage container
 $storagecontainer = New-AzureStorageContainer -Name $storagecontainername `
 -Context $(New-AzureStorageContext -StorageAccountName $storageaccountname `
     -StorageAccountKey $(Get-AzureRmStorageAccountKey -ResourceGroupName $resourcegroupname -StorageAccountName $storageaccountname).Value[0])
+
+Write-host "Created storage container" -f Cyan
 
 # Download sample database from Github
 Invoke-WebRequest -Uri "https://github.com/t1agob/ContosoExpensesARM/raw/master/database/ContosoExpensesDB.bacpac" -OutFile $bacpacfilename
@@ -32,8 +46,15 @@ Set-AzureStorageBlobContent -Container $storagecontainername `
 -Context $(New-AzureStorageContext -StorageAccountName $storageaccountname `
     -StorageAccountKey $storageaccountkey)
 
+Write-host "Successfully uploaded .bacpac to storage account" -f Cyan
+
+# Delete temporary .bacpac file
+Remove-Item .\ContosoExpensesDB.bacpac
+
 # Output storage account name and key
-Write-Host " ----              STORAGE ACCOUNT INFORMATION             ---- " -f Yellow
-Write-Host " STORAGE ACCOUNT NAME: " -NoNewline; Write-Host "$storageaccountname" -f Red
-Write-Host " STORAGE ACCOUNT KEY: " -NoNewline; Write-Host "$storageaccountkey" -f Red
+Write-Host ""
+Write-Host " ----                  DEPLOYMENT INFORMATION                  ---- " -f Yellow
+Write-Host " - RESOURCE GROUP NAME: " -NoNewline; Write-Host "$resourcegroupname" -f Red
+Write-Host " - STORAGE ACCOUNT NAME: " -NoNewline; Write-Host "$storageaccountname" -f Red
+Write-Host " - STORAGE ACCOUNT KEY: " -NoNewline; Write-Host "$storageaccountkey" -f Red
 Write-Host " - KEEP THIS INFORMATION AS IT WILL BE REQUIRED FOR DEPLOYING - " -f Yellow
